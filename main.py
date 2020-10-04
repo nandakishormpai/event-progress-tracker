66fa8b67d0ede040cc233d3729fcfdea5a09aa72
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-10-04 22:11:46 +0530

    renamed variable for better readability

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ for i in range (0,n):
     link=input("Enter the Qwiklab public profile URL here:\n")
     res = requests.get(link) 
     noStarchSoup = bs4.BeautifulSoup(res.text,"html.parser")   
-    elems = noStarchSoup.select('div .public-profile__badge')
+    quests = noStarchSoup.select('div .public-profile__badge')
     name =  (noStarchSoup.select('div .public-profile__hero h1')[0].getText())[1:-1]
     print("\n name:",name)
-    print("number of quests completed is:",len(elems))
-    score[name]=len(elems)
+    print("number of quests completed is:",len(quests))
+    score[name]=len(quests)
 
 print(score)
 
