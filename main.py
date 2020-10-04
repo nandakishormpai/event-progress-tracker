0582765c318252205a3d79cefaa2944840559c87
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-10-04 22:06:36 +0530

    dictionarised name and quest count

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import requests   
 import webbrowser
 import bs4
-link=input("Enter the Qwiklab public profile URL here:\n")
-res = requests.get(link) 
-noStarchSoup = bs4.BeautifulSoup(res.text,"html.parser")   
-elems = noStarchSoup.select('div .public-profile__badge')
-print("number of quests completed is:",len(elems))
+n=int(input("Number of urls\n"))
+score={}
+for i in range (0,n):
+    link=input("Enter the Qwiklab public profile URL here:\n")
+    res = requests.get(link) 
+    noStarchSoup = bs4.BeautifulSoup(res.text,"html.parser")   
+    elems = noStarchSoup.select('div .public-profile__badge')
+    name =  (noStarchSoup.select('div .public-profile__hero h1')[0].getText())[1:-1]
+    print("\n name:",name)
+    print("number of quests completed is:",len(elems))
+    score[name]=len(elems)
+
+print(score)
+
