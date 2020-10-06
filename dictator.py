5cbbc2e0462663e78f2b501263b92a5f39d04ab2
nandakishormpai2001 <57388834+nandakishormpai2001@users.noreply.github.com>
2020-10-06 21:55:25 +0530

    update

--- dictator.py
+++ dictator.py
@@ -1,15 +1,14 @@
 import csv
 
-
 def create_dict():
-    dict={}
+    diction={}
     with open('data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             if (row[0]=="Timestamp"):
                 continue
-            dict[row[1]]=row[2]
-    return dict
+            diction[row[1]]=row[2]
+    return diction
 
 
 create_dict()        
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import requests
 import webbrowser
 import bs4
 from dictator import create_dict
+
 score={}
 quest_list=["Perform Foundational Infrastructure Tasks in Google Cloud","Setup and Configure a Cloud Environment in Google Cloud","Deploy and Manage Cloud Environments with Google Cloud","Build and Secure Networks in Google Cloud","Deploy to Kubernetes in Google Cloud","Getting Started: Create and Manage Cloud Resources","Perform Foundational Data, ML, and AI Tasks in Google Cloud","Insights from Data with BigQuery","Engineer Data in Google Cloud","Integrate with Machine Learning APIs","Explore Machine Learning Models with Explainable AI"]
 def get_board():
     score = create_dict()
+
     for name,qwik in score.items() :
         quest_completed=[]
         quest_count=0
@@ -15,16 +17,17 @@ def get_board():
         quests = noStarchSoup.select('div .public-profile__badge')
         #name =  (noStarchSoup.select('div .public-profile__hero h1')[0].getText())[1:-1]
         badges=noStarchSoup.select('div .public-profile__badge div')
-        j=0
-        for k in badges:
-            if (j%3==1):
-                quest_completed.append(k.getText()[1:-1])
-            j=j+1
+        for i in range (1,len(badges)-1,3):
+            quest_completed.append(badges[i].getText()[1:-1])
         for k in quest_completed:
             if (k in quest_list):
                 quest_count=quest_count+1
+
         score[name]=quest_count
 
     sort_orders = sorted(score.items(), key=lambda x: x[1], reverse=True)
     return sort_orders[0:20]
+
+
+get_board()
 
